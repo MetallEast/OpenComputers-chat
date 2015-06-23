514d99028a950a1bb88fe3884ac9f766a14e2ae7
Vadim Savchenko <hardanger1994@gmail.com>
2015-06-23 18:50:32 +0200

    Main part
    Added server and client code

---
+++ Chat/Client.lua
@@ -0,0 +1,151 @@
+﻿local thread = require("thread")
+local computer = require("computer")
+local component = require("component")
+local term = require("term")
+local text = require("text")
+local serialization = require("serialization")
+local event = require("event")
+local modem = component.modem
+local gpu = component.gpu
+
+local registrationLevel   = "9f54fd85-eec4-4d75-8830-0924570d54bc"
+local authenticationLevel = "9ed56ccb-b4c4-4dba-bb2d-50dccb844942"			
+local primaryLevel	  = "79c5fa93-e6bd-4e97-80cc-193c4b6a1898"
+local primaryPort
+local myMessage
+local A, B
+local name
+
+function Authentication()
+	term.write("Имя пользователя: ")
+	local nickname = text.trim(term.read())
+	term.write("Пароль: ")
+	local password = text.trim(term.read())
+	
+	local user = {}
+	user[1] = nickname
+	user[2] = password
+	
+	local packet = serialization.serialize(user)
+	modem.open(256)
+	modem.send(authenticationLevel, 256, packet)
+	local _, _, address, _, _, message = event.pull("modem_message")
+	modem.close(256)
+	if type(message) == "number" then 
+		name = nickname
+		return message
+	else 
+		print(message)
+		event.pull("key_up")
+		term.clear()
+		return 0
+	end
+end
+
+function Registration()
+	local _, _, _, _, _, message
+	local user = {}
+	term.clear()
+	term.write("Имя пользователя: ")
+	local nickname = text.trim(term.read())
+	term.write("Пароль: ")
+	local password = text.trim(term.read())
+	term.write("Повторите пароль: ")
+	local repetition = text.trim(term.read())
+	if password ~= repetition or string.len(password) < 3 or string.len(password) > 10 then 
+		term.write("\nПароль должен быть в диапазоне от 3 до 10 символов")
+		term.write("\nЗначения полей 'Пароль' и 'Повтор пароля' должны совпадать")
+		os.sleep(1)
+	else 
+		user[1] = nickname
+		user[2] = password
+		local packet = serialization.serialize(user)
+		modem.open(255)
+		modem.send(registrationLevel, 255, packet)
+		_, _, _, _, _, message = event.pull("modem_message")
+		modem.close(255)
+		if type(message) == "number" then
+			term.clear()
+			print("Регистрация завершена")
+		else 
+			print(message) 
+		end
+	end
+	event.pull("key_up")
+	term.clear()
+end
+
+function Choice()
+	local choice
+	local authResult
+	while true do
+		print("1. Войти в чат")
+		print("2. Регистрация")
+		print("3. Выход")
+		choice = text.trim(term.read(nil, false))
+		term.clear()
+		if choice == "1" then 
+			authResult = Authentication() 
+			if authResult ~= 0 then
+				return authResult		
+			end
+		end
+		if choice == "2" then Registration() end
+		if choice == "3" then break end
+	end
+	return 0
+end
+
+function Receiver()
+	local x, y		
+	local _, _, address, _, _, message
+	while true do
+		while address ~= primaryLevel do
+			_, _, address, _, _, message = event.pull("modem_message")
+		end
+		address = nil
+		x, y = term.getCursor()
+		term.setCursor(1, B - 5)
+		print(text.trim(message))
+		term.setCursor(A, B)
+		term.write(' ', true)
+		gpu.copy(1, B - 2, A, 3, 0, 1)
+		term.setCursor(1, B - 2)
+		term.clearLine()
+		term.setCursor(x, B - 1)
+		if message ~= myMessage then computer.beep(1000, 0.1) end
+	end
+end
+
+function Sender()
+	local result
+	local ddot = ": "
+	while true do 
+		myMessage = term.read(nil, false)
+		result = text.trim(myMessage)
+		result = text.detab(result, 1)
+		if result == "exit" then return 0 end
+		if result ~= "" and string.len(myMessage) < 256 then 
+			myMessage = string.format("%s%s%s", name, ddot, myMessage)
+			modem.send(primaryLevel, primaryPort, myMessage) 
+		end
+		term.clearLine()
+	end
+end
+
+
+modem.setStrength(5000)
+local choiceResult = Choice()
+if choiceResult ~= 0 then 
+	primaryPort = choiceResult
+	modem.open(primaryPort)
+	A, B = gpu.getResolution()
+	term.clear()
+	term.setCursor(1, B - 1)
+	thread.init()
+	local handler = thread.create(Receiver)
+	Sender()
+	modem.close(choiceResult)
+	thread.kill(handler)
+	thread.waitForAll()
+end
---
+++ Chat/Server.lua
@@ -0,0 +1,116 @@
+﻿local thread = require("thread")
+local component = require("component")
+local event = require("event")
+local serialization = require("serialization")
+local text = require("text")
+
+local modemPrim = component.proxy("79c5fa93-e6bd-4e97-80cc-193c4b6a1898")
+local modemAuth = component.proxy("9ed56ccb-b4c4-4dba-bb2d-50dccb844942")
+local modemReg  = component.proxy("9f54fd85-eec4-4d75-8830-0924570d54bc")
+local primaryPort = math.random(512, 1024)
+
+
+function RegistrationLevel()
+	local _, _, address, port, _, message
+	while true do
+		while port ~= 255 do
+			_, _, address, port, _, message = event.pull("modem_message")
+		end
+		port = nil
+		local newUser = serialization.unserialize(message)
+		if string.len(newUser[1]) > 15 or string.len(newUser[1]) < 3 then 
+			modemReg.send(address, 255, "Длина имени пользователя должна быть от 3 до 15 символов")
+		else
+			local file = io.open("users", "a")
+			io.output(file)
+			local line
+			file:seek("set")
+			while true do
+				line = file:read()
+				file:read()
+				if newUser[1] == line then
+					modemReg.send(address, 255, "Пользователь с таким именем уже существует")
+					break
+				end		
+				if line == nil then
+					io.input(file)
+					file:seek("end")
+					file:write("\n")
+					file:write(newUser[1])
+					file:write("\n")
+					file:write(newUser[2])
+					modemReg.send(address, 255, 1)		
+					break
+				end 	
+			end
+			file:close(file)
+		end
+	end
+end
+
+function AuthenticationLevel()
+	local _, _, address, port, _, message
+	local line
+	while true do
+		while port ~= 256 do
+			_, _, address, port, _, message = event.pull("modem_message")
+		end
+		port = nil
+		local user = serialization.unserialize(message)
+		local file = io.open("users", "r")
+		io.output(file)
+		file:seek("set")
+		while true do
+			line = file:read()
+			if user[1] == line then
+				line = file:read()
+				if user[2] == line then 				
+					 modemAuth.send(address, 256, primaryPort)	
+				else modemAuth.send(address, 256, "Неверный пароль")						
+				end
+				break
+			end		
+			if line == nil then 					
+				modemAuth.send(address, 256, "Пользователя с таким именем не существует")
+				break
+			end 			 
+		end
+		file:close(file)
+	end
+end
+
+function PrimaryLevel()
+	local check
+	local _, _, address, port = nil, _, message
+	while true do
+		while port ~= primaryPort do
+			_, _, address, port, _, message = event.pull("modem_message")
+			print("Address:", address, "\nPort:\t", port, "\nMessage:", message)
+		end
+		port = nil
+		check = text.trim(message)
+		check = text.detab(check, 1)
+		if check ~= "" and string.len(message) < 256 then
+			modemPrim.broadcast(primaryPort, message)
+		end
+	end
+end
+
+
+modemReg.open(255)
+modemAuth.open(256)
+modemPrim.open(primaryPort)
+modemReg.setStrength(5000)
+modemAuth.setStrength(5000)
+modemPrim.setStrength(5000)
+
+thread.init()
+thread.create(RegistrationLevel)
+thread.create(AuthenticationLevel)
+PrimaryLevel()
+
+modemReg.close(255)
+modemAuth.close(256)
+modemPrim.close(primaryPort)
+thread.killAll()
+thread.waitForAll()
